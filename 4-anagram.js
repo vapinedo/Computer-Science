6b46c5393ae2e4564d1d45eed547e0e7abbd3950
Victor Pinedo <victor.pinedo@sailpoint.com>
2023-05-17 09:47:35 -0500

    Anagram solution 2

--- 4-anagram.js
+++ 4-anagram.js
@@ -3,8 +3,6 @@
 ************************/
 
 function validAnagram(str1, str2){
-  // add whatever parameters you deem necessary - good luck!
-  
   if (str1.length !== str2.length) { return false; }
   
   let lowerStr1 = str1.toLowerCase();
@@ -21,24 +19,47 @@ function validAnagram(str1, str2){
   for (let char of lowerStr2) {
       freqCounter2[char] = (freqCounter2[char] || 0) + 1;
   }
-  
-  // console.log(freqCounter1);
-  // console.log(freqCounter2);
     
   for (let key in freqCounter1) {
     if (!(key in freqCounter2)) { return false; } 
     if (freqCounter1[key] !== freqCounter2[key]) { return false; }
   }
   
+  return true;    
+}
+
+/************************
+    Soluction 2
+************************/
+
+function validAnagram2(first, second) {
+  if (first.length !== second.length) { return false; }
+
+  const lookup = {};
+
+  for (let i = 0; i < first.length; i++) {
+    let letter = first[i];
+    lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1; 
+  }
+
+  for (let i=0; i<second.length; i++) {
+    let letter = second[i];
+
+    if (lookup[letter]) {
+      lookup[letter] -= 1;
+    } else {
+      return false;
+    }
+  }
+
   return true;
-    
 }
   
-console.log(validAnagram('', '')); // true
-console.log(validAnagram('aaz', 'zza')); // false
-console.log(validAnagram('anagram', 'nagaram')); // true
-console.log(validAnagram("rat","car")); // false
-console.log(validAnagram('awesome', 'awesom')); // false
-console.log(validAnagram('amanaplanacanalpanama', 'acanalmanplanpamana')); // false
-console.log(validAnagram('qwerty', 'qeywrt')); // true
-console.log(validAnagram('texttwisttime', 'timetwisttext')); // true
+console.log(validAnagram2('', '')); // true
+console.log(validAnagram2('aaz', 'zza')); // false
+console.log(validAnagram2('anagram', 'nagaram')); // true
+console.log(validAnagram2("rat","car")); // false
+console.log(validAnagram2('awesome', 'awesom')); // false
+console.log(validAnagram2('amanaplanacanalpanama', 'acanalmanplanpamana')); // false
+console.log(validAnagram2('qwerty', 'qeywrt')); // true
+console.log(validAnagram2('texttwisttime', 'timetwisttext')); // true
