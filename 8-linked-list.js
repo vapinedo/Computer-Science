d784b23ff05236ae53a3e09bd1ef7f13b9d1fec8
Victor Pinedo <victor.pinedo@sailpoint.com>
2023-05-24 11:40:09 -0500

    add Push method on linked list

--- 8-linked-list.js
+++ 8-linked-list.js
@@ -12,6 +12,24 @@ class LinkedList {
         this.tail = this.head;
         this.length = 1;
     }
+
+    push(value) {
+        let newNode = new Node(value);
+
+        // check if linked list is emtpy
+        if (!this.head) { 
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode; 
+            this.tail = newNode;
+        }
+
+        this.length++;
+        return this;
+    }
 }
 
-let firstLL = new LinkedList(4);
+let myLinkedList = new LinkedList(7);
+myLinkedList.push(4);
+console.dir(myLinkedList);
